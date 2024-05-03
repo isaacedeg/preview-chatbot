dca0f5eb715d61433198dfedb173da1fadef539e
isaacedeg <139331514+isaacedeg@users.noreply.github.com>
2024-05-03 17:58:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import string
 import streamlit as st
 
 # Load the text file and preprocess the data
-with open('clean_output.txt', 'r', encoding='utf-8') as f:
+with open('file/clean_output.txt', 'r', encoding='utf-8') as f:
     data = f.read().replace('\n', ' ')
     
 # Tokenize the text into sentences
